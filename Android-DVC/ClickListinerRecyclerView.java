d7241dd236b1d60839ae2889be5409c10bfd9a8f
DavidCenturion22 <davidverace22@gmail.com>
2025-09-25 10:27:35 -0600

    Commit
    Commit

---
+++ Android-DVC/ClickListinerRecyclerView.java
@@ -0,0 +1,10 @@
+package com.example.recyclerviewsejemplo;
+
+
+public abstract class ClickListiner{
+
+    // here index is index
+// of item clicked
+    public abstract void click(int index);
+
+}
